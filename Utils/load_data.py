cfd25cb21aec1d81b8672986f39945e5c0b70e06
Javier Jiménez Rodríguez <javier.jimenez02@estudiante.uam.es>
2022-05-21 19:48:18 +0200

    Renamed dataset_utils' module into load_data's module and hotfixes have been made
    · Fixed circular imports
    · Added private functions
    · Applied clean code conventions (private functions to the end of the file, snake case in Python & functions refactorization)

--- Utils/load_data.py
+++ Utils/load_data.py
@@ -1,39 +1,12 @@
-from sktime.datasets import load_from_tsfile_to_dataframe
 import os
 import pandas as pd
 import numpy as np
-from itertools import zip_longest
 from utils.data_extraction import Data
 from scipy.io import arff
 from sktime.datatypes._panel._convert import (from_pd_wide_to_nested,
                                               from_nested_to_multi_index)
-
-
-def from_nested_to_long_class(X, y):
-    '''
-    This method will extend all "y"'s vector values for each instance
-    of "X"'s DataFrame.
-    '''
-    new_y = []
-
-    for serie_id, serie_class in zip_longest(pd.unique(X['id']), y):
-        new_y.extend([serie_class for _ in range(
-            X[X['id'] == serie_id].shape[0])])
-
-    return np.asarray(new_y)
-
-
-def __extract_heartbeat_dimension(path: str, dim_name: str):
-    actual_dim = pd.DataFrame(arff.loadarff(path)[0])
-    y = actual_dim.pop('target')
-    actual_dim = from_nested_to_multi_index(from_pd_wide_to_nested(
-        actual_dim)).reset_index(level=[0, 1])
-    actual_dim = actual_dim.rename(
-        {'instance': 'id',
-         'timepoints': 'TimeStamp',
-         0: 'signal_' + dim_name}, axis=1)
-
-    return actual_dim, y
+from sktime.datasets import load_from_tsfile_to_dataframe
+from itertools import zip_longest
 
 
 def import_heartbeat_dataset(heartbeat_path: str):
@@ -70,8 +43,8 @@ def import_heartbeat_dataset(heartbeat_path: str):
                         left_on=['id', 'TimeStamp'],
                         right_on=['id', 'TimeStamp'])
 
-    y_train = from_nested_to_long_class(train, y_train)
-    y_test = from_nested_to_long_class(test, y_test)
+    y_train = __from_nested_to_long_class(train, y_train)
+    y_test = __from_nested_to_long_class(test, y_test)
 
     train.insert(train.shape[1], 'class', y_train)
     test.insert(test.shape[1], 'class', y_test)
@@ -93,35 +66,13 @@ def import_epilepsy_dataset(epilepsy_path: str):
     test_df = test_df.rename(
         {'instance': 'id', 'timepoints': 'TimeStamp'}, axis=1)
 
-    y_train = from_nested_to_long_class(train_df, y_train)
-    y_test = from_nested_to_long_class(test_df, y_test)
+    y_train = __from_nested_to_long_class(train_df, y_train)
+    y_test = __from_nested_to_long_class(test_df, y_test)
 
     train_df.insert(train_df.shape[1], "class", y_train)
     test_df.insert(test_df.shape[1], "class", y_test)
 
     return Data(train_df), Data(test_df)
-
-
-def __seguimiento_ocular_parse_file(
-        relative_path,
-        file_prefix,
-        folder_id,
-        file_id,
-        file_ext):
-    serie_file = relative_path + file_prefix + str(file_id) + file_ext
-    serie_data = pd.read_csv(serie_file, delimiter='\t')
-
-    # Hard-coded into Seguimiento Ocular's specific case
-    with open(relative_path + 'class.txt') as class_file:
-        class_data = class_file.readline()
-    serie_class, serie_diagnosis, age = class_data.split('-')
-
-    serie_data.insert(0, 'id', str(folder_id) + '-' + str(file_id))
-    serie_data.insert(2, 'Age', age)
-    serie_data.insert(serie_data.shape[1], 'class', serie_class)
-    serie_data.insert(serie_data.shape[1], 'diagnosis', serie_diagnosis)
-
-    return serie_data
 
 
 def import_seguimiento_ocular_dataset(
@@ -144,3 +95,52 @@ def import_seguimiento_ocular_dataset(
             all_data = all_data.append(file_data, ignore_index=True)
 
     return Data(all_data)
+
+
+def __from_nested_to_long_class(X, y, id_col_name='id'):
+    '''
+    This method will extend all "y"'s vector values for each instance
+    of "X"'s DataFrame.
+    '''
+    new_y = []
+
+    for serie_id, serie_class in zip_longest(pd.unique(X[id_col_name]), y):
+        new_y.extend([serie_class for _ in range(
+            X[X[id_col_name] == serie_id].shape[0])])
+
+    return np.asarray(new_y)
+
+
+def __extract_heartbeat_dimension(path: str, dim_name: str):
+    actual_dim = pd.DataFrame(arff.loadarff(path)[0])
+    y = actual_dim.pop('target')
+    actual_dim = from_nested_to_multi_index(from_pd_wide_to_nested(
+        actual_dim)).reset_index(level=[0, 1])
+    actual_dim = actual_dim.rename(
+        {'instance': 'id',
+         'timepoints': 'TimeStamp',
+         0: 'signal_' + dim_name}, axis=1)
+
+    return actual_dim, y
+
+
+def __seguimiento_ocular_parse_file(
+        relative_path,
+        file_prefix,
+        folder_id,
+        file_id,
+        file_ext):
+    serie_file = relative_path + file_prefix + str(file_id) + file_ext
+    serie_data = pd.read_csv(serie_file, delimiter='\t')
+
+    # Hard-coded into Seguimiento Ocular's specific case
+    with open(relative_path + 'class.txt') as class_file:
+        class_data = class_file.readline()
+    serie_class, serie_diagnosis, age = class_data.split('-')
+
+    serie_data.insert(0, 'id', str(folder_id) + '-' + str(file_id))
+    serie_data.insert(2, 'Age', age)
+    serie_data.insert(serie_data.shape[1], 'class', serie_class)
+    serie_data.insert(serie_data.shape[1], 'diagnosis', serie_diagnosis)
+
+    return serie_data
